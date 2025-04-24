e926989a5d7b82296232f99185573caf1430db2e
Olya Mateshov <olyamateshov@gmail.com>
2025-04-24 14:45:45 -0400

    Wave 3

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,12 +1,20 @@
-class Planet:
-    def __init__(self, id, name, description, distance):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.distance = distance
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    description: Mapped[str]
+
+
+#     def __init__(self, id, name, description, distance):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.distance = distance
     
-planets = [
-    Planet(1, "Mars", "Dusty, desolate, red planet", "fourth planet from the Sun" ),
-    Planet(2, "Venus", "Hot, hostile, clouded planet", "second planet from the Sun"),
-    Planet(3, "Jupiter", "Massive, stormy, gas giant planet", "fifth planet from the Sun")
-]
+# planets = [
+#     Planet(1, "Mars", "Dusty, desolate, red planet", "fourth planet from the Sun" ),
+#     Planet(2, "Venus", "Hot, hostile, clouded planet", "second planet from the Sun"),
+#     Planet(3, "Jupiter", "Massive, stormy, gas giant planet", "fifth planet from the Sun")
+# ]
--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,43 +1,43 @@
-from flask import Blueprint, abort, make_response
-from ..models.planet import planets
+from flask import Blueprint, abort, make_response, jsonify
+# from ..models.planet import planets
 
 planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
 
-@planet_bp.get("")
-def get_all_planets():
-    planet_response = []
-    for planet in planets:
-        planet_response.append(dict(
-            id = planet.id,
-            name = planet.name,
-            description = planet.description,
-            distance = planet.distance
-        ))
-    return planet_response
+# @planet_bp.get("")
+# def get_all_planets():
+#     planet_response = []
+#     for planet in planets:
+#         planet_response.append(dict(
+#             id = planet.id,
+#             name = planet.name,
+#             description = planet.description,
+#             distance = planet.distance
+#         ))
+#     return jsonify(planet_response)
 
-@planet_bp.get("/<planet_id>")
-def get_one_planet(planet_id):
-    planet = validate_planet(planet_id)
+# @planet_bp.get("/<planet_id>")
+# def get_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
 
-    return dict(
-            id = planet.id,
-            name = planet.name,
-            description = planet.description,
-            distance = planet.distance
-        )
+#     return dict(
+#             id = planet.id,
+#             name = planet.name,
+#             description = planet.description,
+#             distance = planet.distance
+#         )
 
 
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except ValueError:
-        response = {"message" : f"planet {planet_id} invalid"}
-        abort(make_response(response, 400))
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except ValueError:
+#         response = {"message" : f"planet {planet_id} invalid"}
+#         abort(make_response(response, 400))
 
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
         
-    response = {"message" : f"planet {planet_id} not found"}
-    abort(make_response(response, 404))
+#     response = {"message" : f"planet {planet_id} not found"}
+#     abort(make_response(response, 404))
 
